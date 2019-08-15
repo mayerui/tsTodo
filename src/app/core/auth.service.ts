847e4961e7ad35a47af249f8a74045b0d6e31c7f
mohuang <mayerui@live.com>
2019-08-16 00:19:18 +0800

    将账号密码改为Map类，支持动态加

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -3,12 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+    constructor()
+    {
+        this.auth_map = new Map();
+        this.auth_map.set('admin', '123456');
+    }
 
     loginCredentials(username: string, password: string): boolean
     {
-        if (username === 'admin' && password === '123456')
+        if (this.auth_map.get(username) === password)
             return true;
         return false;
     }
+
+    auth_map: Map<string, string>;
 }
